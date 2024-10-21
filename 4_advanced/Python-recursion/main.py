f4759734f55032e1d75f555f9f218dfcabd22463
vinald <okiror1vinald@gmail.com>
2024-10-21 23:49:07 +0300

    Error handling and Functions

---
+++ 4_advanced/Python-recursion/main.py
@@ -0,0 +1,10 @@
+def find_sum(n):
+    if n == 1:
+        return 1
+    return n + find_sum(n - 1)
+
+if __name__ == "__main__":
+    print(find_sum(3))  # 6
+    print(find_sum(5))  # 15
+    print(find_sum(10))  # 55
+    print(find_sum(100))  # 5050
